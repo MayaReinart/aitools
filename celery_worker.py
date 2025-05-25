1e0ee1a4651f627077bf294bde5a015ff8ffe377
Maya Reinart <mayareinart@gmail.com>
2025-05-25 18:26:48 +0000

    Added celery_worker draft

---
+++ celery_worker.py
@@ -0,0 +1,10 @@
+from celery import Celery
+from .src.core.config import settings
+
+celery_app = Celery(
+    "summarizer",
+    broker=settings.REDIS_URL,
+    backend=settings.REDIS_URL,
+)
+
+celery_app.autodiscover_tasks(["tasks"])
